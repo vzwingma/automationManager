b016cdd906b53a028c5e02851b0f7cb16afcd83a
Vincent ZWINGMANN <vincent.zwingmann@gmail.com>
2016-08-21 14:08:26 +0200

    correction répertoires

--- src/main/resources/feature_github_generation.sh
+++ src/main/resources/feature_github_generation.sh
@@ -2,7 +2,6 @@
 TRAVIS_BRANCH=1.2.0-RC1
 echo "Génération du fichier de features pour les releases GitHub"
 echo "Release GitHub : $TRAVIS_BRANCH"
-# cd src/main/resources/template
 cd template/
 # Remplacement de ${project.version} par le n° de release GitHub
 sed -i -e "s/\${project.version}/$TRAVIS_BRANCH/g" feature.xml
