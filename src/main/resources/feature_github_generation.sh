5b95d3c90d4285affaf72940e2d8bd3426c18287
Vincent ZWINGMANN <vincent.zwingmann@gmail.com>
2016-08-21 14:43:49 +0200

    maj du script de push

--- src/main/resources/feature_github_generation.sh
+++ src/main/resources/feature_github_generation.sh
@@ -1,11 +1,10 @@
 #/bin/bash
-TRAVIS_BRANCH=1.2.0-RC1
 echo "Génération du fichier de features pour les releases GitHub"
 echo "Release GitHub : $TRAVIS_BRANCH"
 cd template/
 # Remplacement de ${project.version} par le n° de release GitHub
 sed -i -e "s/\${project.version}/$TRAVIS_BRANCH/g" feature.xml
-# Todo : Doit être recommité sur master
+
 sed -i -e "s/v\${project.version}/$TRAVIS_BRANCH/g" README.md
 # Push sur Master
 cd ..
