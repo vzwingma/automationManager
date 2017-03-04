3fc8c16f03e5edbd3532c5c60f96e611ac43e9ae
vzwingma <vincent.zwingmann@gmail.com>
2017-03-04 23:37:53 +0100

    #10 : correction des patterns UBER

--- automationCommons/src/main/java/com/terrier/utilities/automation/bundles/communs/utils/replace/ReplaceDatePattern.java
+++ automationCommons/src/main/java/com/terrier/utilities/automation/bundles/communs/utils/replace/ReplaceDatePattern.java
@@ -8,14 +8,18 @@ import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author vzwingma
  *
  */
 public class ReplaceDatePattern implements IReplacePattern {
 
-	
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger( ReplaceDatePattern.class );
+
 	/* (non-Javadoc)
 	 * @see com.terrier.utilities.automation.bundles.communs.utils.replace.IReplacePattern#replace(java.lang.String)
 	 */
@@ -23,13 +27,19 @@ public class ReplaceDatePattern implements IReplacePattern {
 	public String replace(String chaineSource, String pattern) {
 
 		Pattern p = Pattern.compile(".*"+PATTERN_IN+"{2} *(.*) *"+PATTERN_OUT+"{2}.*");
-        Matcher m = p.matcher(pattern);
-        if(m.find()){
-        	String patternDate = m.group(1);
-        	SimpleDateFormat sdf = new SimpleDateFormat(patternDate);
-        	String date = sdf.format(Calendar.getInstance().getTime());
-        	chaineSource = pattern.replaceAll(PATTERN_IN + PATTERN_IN + patternDate + PATTERN_OUT + PATTERN_OUT, date);
-        }
+		Matcher m = p.matcher(pattern);
+		if(m.find()){
+			String patternDate = m.group(1);
+			try{
+				SimpleDateFormat sdf = new SimpleDateFormat(patternDate);
+
+				String date = sdf.format(Calendar.getInstance().getTime());
+				chaineSource = pattern.replaceAll(PATTERN_IN + PATTERN_IN + patternDate + PATTERN_OUT + PATTERN_OUT, date);
+			}
+			catch(IllegalArgumentException e){
+				LOGGER.warn("Le pattern {} n'est pas un format de date", m.group(1));
+			}
+		}
 		return chaineSource;
 	}
 
