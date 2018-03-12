3110929fa5be309434265c0b9415a8f740143ac3
Vincent ZWINGMANN <vincent.zwingmann@soprasteria.com>
2018-03-12 18:58:04 +0100

    Ajout de l'auth

--- emailsWorkerBundle/src/main/java/com/terrier/utilities/automation/bundles/emails/worker/business/EmailsWorkerBusinessService.java
+++ emailsWorkerBundle/src/main/java/com/terrier/utilities/automation/bundles/emails/worker/business/EmailsWorkerBusinessService.java
@@ -5,16 +5,18 @@ package com.terrier.utilities.automation.bundles.emails.worker.business;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.terrier.utilities.automation.bundles.communs.business.AbstractAutomationService;
 import com.terrier.utilities.automation.bundles.communs.model.StatutPropertyBundleObject;
 
 /**
- * Service métier du worker
+ * Service mï¿½tier du worker
  * @author PVZN02821
  *
  */
@@ -22,7 +24,22 @@ import com.terrier.utilities.automation.bundles.communs.model.StatutPropertyBund
 public class EmailsWorkerBusinessService extends AbstractAutomationService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger( EmailsWorkerBusinessService.class );
+	
+	public static final String CONFIG_PID = "com.terrier.utilities.automation.bundles.emails.worker";
 
+	
+	/* (non-Javadoc)
+	 * @see com.terrier.utilities.automation.bundles.communs.business.AbstractAutomationService#startService()
+	 */
+	@PostConstruct
+	public void startService() {
+		// Register config
+		super.registerToConfig(CONFIG_PID);
+		
+		
+		
+	}
+	
 	@Override
 	public void notifyUpdateDictionary() {
 
